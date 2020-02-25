236da18e58f9a41df902339d8386bee5659a5f58
Luis David Sáenz Jurado <luisdavidspider@gmail.com>
2020-02-25 02:28:29 -0600

    Programando logoutScript
    Se programo archivo logoutScript para el cierre de sesiones

--- vistas/modulos/logoutScript.php
+++ vistas/modulos/logoutScript.php
@@ -2,17 +2,32 @@
 $(document).ready(function(){
     $('.btn-exit-system').on('click', function(e){
 		e.preventDefault();
+		var Token=$(this).attr('href');
 		swal({
-		  	title: 'Are you sure?',
-		  	text: "The current session will be closed",
+		  	title: '¿Estás seguro?',
+		  	text: "La sesión actual se cerrará y deberás iniciar sesión nuevamente",
 		  	type: 'warning',
 		  	showCancelButton: true,
 		  	confirmButtonColor: '#03A9F4',
 		  	cancelButtonColor: '#F44336',
-		  	confirmButtonText: '<i class="zmdi zmdi-run"></i> Yes, Exit!',
-		  	cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, Cancel!'
+		  	confirmButtonText: '<i class="zmdi zmdi-run"></i> Si, cerrar!',
+		  	cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, Cancelar!'
 		}).then(function () {
-			window.location.href="index.html";
+			$.ajax({
+				url:'<?php echo SERVERURL; ?>ajax/loginAjax.php?Token='+Token,
+				success:function(data){
+					if(data=="true"){
+						window.location.href="<?php echo SERVERURL; ?>login/";
+					}else{
+						swal(
+							"Ocurrió un error",
+							"No se pudo cerrar la sesión",
+							"error"
+						);
+					}
+				}
+				
+			});
 		});
 	});
 });
