8a1daaf161357305377931d57b7690f7a9c36dbe
Luis David Sáenz Jurado <luisdavidspider@gmail.com>
2020-02-26 16:32:09 -0600

    Seleccion de datos cuenta 2/3
    Seleccion de datos cuenta 2/3

--- vistas/contenidos/myaccount-view.php
+++ vistas/contenidos/myaccount-view.php
@@ -1,148 +1,203 @@
-<!-- Content Page -->
+<!-- Content page -->
 <div class="container-fluid">
-    <div class="page-header">
-        <h1 class="text-titles"><i class="zmdi zmdi-settings zmdi-hc-fw"></i> MI CUENTA</small></h1>
-    </div>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse voluptas reiciendis tempora voluptatum eius porro ipsa quae voluptates officiis sapiente sunt dolorem, velit quos a qui nobis sed, dignissimos possimus!</p>
+	<div class="page-header">
+	  <h1 class="text-titles"><i class="zmdi zmdi-settings zmdi-hc-fw"></i> MI CUENTA</small></h1>
+	</div>
+	<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse voluptas reiciendis tempora voluptatum eius porro ipsa quae voluptates officiis sapiente sunt dolorem, velit quos a qui nobis sed, dignissimos possimus!</p>
 </div>
 
 <?php
-    $datos=explode("/",$_GET['views']);
-    
-    if(isset($datos[1]) && ($datos[1]=="admin" || $datos[1]=="user")):
+	$datos=explode("/", $_GET['views']);
 
-    require_once "./controladores/cuentaControlador.php";
-    $classCuenta = new cuentaControlador();
+	if(isset($datos[1]) && ($datos[1]=="admin" || $datos[1]=="user")):
 
-    $filesC=$classCuenta->datos_cuenta_controlador($datos[2]);
+		require_once "./controladores/cuentaControlador.php";
+		$classCuenta = new cuentaControlador();
 
-    if($filesC->rowCount()==1){
+		$filesC=$classCuenta->datos_cuenta_controlador($datos[2]);
+
+		if($filesC->rowCount()==1){
 ?>
 <div class="container-fluid">
-    <div class="panel panel-success">
-        <div class="panel-heading">
-            <h3 class="panel-title"><i class="zmdi zmdi-refresh"></i> &nbsp; MI CUENTA</h3>
-        </div>
-        <div class="panel-body">
+	<div class="panel panel-success">
+		<div class="panel-heading">
+			<h3 class="panel-title"><i class="zmdi zmdi-refresh"></i> &nbsp; MI CUENTA</h3>
+		</div>
+		<div class="panel-body">
             <form action="<?php echo SERVERURL; ?>ajax/cuentaAjax.php" method="POST" data-form="update" class="FormularioAjax" autocomplete="off" enctype="multipart/form-data">
-                <fieldset>
-                    <legend><i class="zmdi zmdi-key"></i> &nbsp; Datos de la cuenta</legend>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Nombre de usuario *</label>
-                                    <input pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{1,15}" class="form-control" type="text" name="usuario-up" required="" maxlength="15">
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-6">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">E-mail</label>
-                                    <input class="form-control" type="email" name="email-up" maxlength="50">
-                                </div>
-                            </div>
-                            <div class="col-xs-12">
-                                <div class="form-group">
-                                    <label class="control-label">Genero</label>
-                                    <div class="radio radio-primary">
-                                        <label>
-                                            <input type="radio" name="optionsGenero" id="optionsRadios1" value="Masculino" checked="">
-                                            <i class="zmdi zmdi-male-alt"></i> &nbsp; Masculino
-                                        </label>
-                                    </div>
-                                    <div class="radio radio-primary">
-                                        <label>
-                                            <input type="radio" name="optionsGenero" id="optionsRadios2" value="Femenino">
-                                            <i class="zmdi zmdi-female"></i> &nbsp; Femenino
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset>
-                <br>
-                <fieldset>
-                    <legend><i class="zmdi zmdi-lock"></i> &nbsp; Contraseña</legend>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo minima cupiditate tempore nobis. Dolor, blanditiis, mollitia. Alias fuga fugiat molestias debitis odit, voluptatibus explicabo quia sequi doloremque numquam dignissimos quis.
-                    </p>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Contraseña actual *</label>
-                                    <input class="form-control" type="password" name="password-up" required="" maxlength="70">
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-6">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Nueva contraseña *</label>
-                                    <input class="form-control" type="password" name="newPassword1-up" required="" maxlength="70">
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-6">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Repita la nueva contraseña *</label>
-                                    <input class="form-control" type="password" name="newPassword2-up" required="" maxlength="70">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset>
-                <br>
-                <fieldset>
-                    <legend><i class="zmdi zmdi-star"></i> &nbsp; Nivel de privilegios</legend>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-6">
-                                <p class="text-left">
-                                    <div class="label label-success">Nivel 1</div> Control total del sistema
-                                </p>
-                                <p class="text-left">
-                                    <div class="label label-primary">Nivel 2</div> Permiso para registro y actualización
-                                </p>
-                                <p class="text-left">
-                                    <div class="label label-info">Nivel 3</div> Permiso para registro
-                                </p>
-                            </div>
-                            <div class="col-xs-12 col-sm-6">
-                                <div class="radio radio-primary">
-                                    <label>
-                                        <input type="radio" name="optionsPrivilegio" id="optionsRadios1" value="1">
-                                        <i class="zmdi zmdi-star"></i> &nbsp; Nivel 1
-                                    </label>
-                                </div>
-                                <div class="radio radio-primary">
-                                    <label>
-                                        <input type="radio" name="optionsPrivilegio" id="optionsRadios2" value="2">
-                                        <i class="zmdi zmdi-star"></i> &nbsp; Nivel 2
-                                    </label>
-                                </div>
-                                <div class="radio radio-primary">
-                                    <label>
-                                        <input type="radio" name="optionsPrivilegio" id="optionsRadios3" value="3" checked="">
-                                        <i class="zmdi zmdi-star"></i> &nbsp; Nivel 3
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset>
-                <p class="text-center" style="margin-top: 20px;">
-                    <button type="submit" class="btn btn-success btn-raised btn-sm"><i class="zmdi zmdi-refresh"></i> Actualizar</button>
-                </p>
-                <div class="RespuestaAjax"></div>
-            </form>
-        </div>
-    </div>
+                <?php
+                    if($_SESSION['codigo_cuenta_sbp']!=$campos['CuentaCodigo']){
+                        if($_SESSION['tipo_sbp']!="Administrador" || $_SESSION['privilegio_sbp']<1 || $_SESSION['privilegio_sbp']>2){
+                            echo $lc->forzar_cierre_sesion_controlador();
+                        }else{
+                            echo '<input type="hidden" name="privilegio-up" value="verdadero">';
+                        }
+                    }
+                ?>
+                <input type="hidden" name="CodigoCuenta-up" value="<?php echo$datos[2]; ?>">
+                <input type="hidden" name="tipoCuenta-up" value="<?php echo $lc->encryption($datos[1]); ?>">
+		    	<fieldset>
+		    		<legend><i class="zmdi zmdi-key"></i> &nbsp; Datos de la cuenta</legend>
+		    		<div class="container-fluid">
+		    			<div class="row">
+		    				<div class="col-xs-12 col-sm-6">
+					    		<div class="form-group label-floating">
+								  	<label class="control-label">Nombre de usuario</label>
+								  	<input pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{1,15}" class="form-control" type="text" name="usuario-up" value="<?php echo$campos['CuentaUsuario']; ?>" required="" maxlength="15">
+								</div>
+		    				</div>
+		    				<div class="col-xs-12 col-sm-6">
+								<div class="form-group label-floating">
+								  	<label class="control-label">E-mail</label>
+								  	<input class="form-control" type="email" name="email-up" maxlength="50">
+								</div>
+		    				</div>
+		    				<div class="col-xs-12 col-sm-6">
+								<div class="form-group">
+									<label class="control-label">Genero</label>
+									<div class="radio radio-primary">
+										<label>
+											<input type="radio" name="optionsGenero-up" value="Masculino" >
+											<i class="zmdi zmdi-male-alt"></i> &nbsp; Masculino
+										</label>
+									</div>
+									<div class="radio radio-primary">
+										<label>
+											<input type="radio" name="optionsGenero-up" value="Femenino" >
+											<i class="zmdi zmdi-female"></i> &nbsp; Femenino
+										</label>
+									</div>
+								</div>
+		    				</div>
+							<div class="col-xs-12 col-sm-6">
+								<div class="form-group">
+									<label class="control-label">Estado de la cuenta</label>
+									<div class="radio radio-primary">
+										<label>
+											<input type="radio" name="optionsEstado-up" value="Activo" >
+											<i class="zmdi zmdi-lock-open"></i> &nbsp; Activo
+										</label>
+									</div>
+									<div class="radio radio-primary">
+										<label>
+											<input type="radio" name="optionsEstado-up" value="Deshabilitado"  >
+											<i class="zmdi zmdi-lock"></i> &nbsp; Deshabilitado
+										</label>
+									</div>
+								</div>
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</fieldset>
+		    	<br>
+		    	<fieldset>
+		    		<legend><i class="zmdi zmdi-lock"></i> &nbsp; Actualizar Contraseña</legend>
+		    		<p>
+		    			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo minima cupiditate tempore nobis. Dolor, blanditiis, mollitia. Alias fuga fugiat molestias debitis odit, voluptatibus explicabo quia sequi doloremque numquam dignissimos quis.
+		    		</p>
+		    		<div class="container-fluid">
+		    			<div class="row">
+		    				<div class="col-xs-12 col-sm-6">
+								<div class="form-group label-floating">
+								  	<label class="control-label">Nueva contraseña *</label>
+								  	<input class="form-control" type="password" name="newPassword1-up" maxlength="50">
+								</div>
+		    				</div>
+		    				<div class="col-xs-12 col-sm-6">
+								<div class="form-group label-floating">
+								  	<label class="control-label">Repita la nueva contraseña *</label>
+								  	<input class="form-control" type="password" name="newPassword2-up" maxlength="50">
+								</div>
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</fieldset>
+		    	<br>
+		    	<fieldset>
+		    		<legend><i class="zmdi zmdi-star"></i> &nbsp; Nivel de privilegios</legend>
+		    		<div class="container-fluid">
+		    			<div class="row">
+		    				<div class="col-xs-12 col-sm-6">
+					    		<p class="text-left">
+			                        <div class="label label-success">Nivel 1</div> Control total del sistema
+			                    </p>
+			                    <p class="text-left">
+			                        <div class="label label-primary">Nivel 2</div> Permiso para registro y actualización
+			                    </p>
+			                    <p class="text-left">
+			                        <div class="label label-info">Nivel 3</div> Permiso para registro
+			                    </p>
+		    				</div>
+		    				<div class="col-xs-12 col-sm-6">
+								<div class="radio radio-primary">
+									<label>
+										<input type="radio" name="optionsPrivilegio-up" >
+										<i class="zmdi zmdi-star"></i> &nbsp; Nivel 1
+									</label>
+								</div>
+								<div class="radio radio-primary">
+									<label>
+										<input type="radio" name="optionsPrivilegio-up" >
+										<i class="zmdi zmdi-star"></i> &nbsp; Nivel 2
+									</label>
+								</div>
+								<div class="radio radio-primary">
+									<label>
+										<input type="radio" name="optionsPrivilegio-up" >
+										<i class="zmdi zmdi-star"></i> &nbsp; Nivel 3
+									</label>
+								</div>
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</fieldset>
+				<br>
+				<fieldset>
+		    		<legend><i class="zmdi zmdi-account-circle"></i> &nbsp; Datos de la cuenta</legend>
+		    		<p>
+						Para poder actualizar los datos de la cuenta por favor ingrese su nombre de usuario y contraseña.
+		    		</p>
+		    		<div class="container-fluid">
+		    			<div class="row">
+		    				<div class="col-xs-12 col-sm-6">
+								<div class="form-group label-floating">
+								  	<label class="control-label">Nombre de usuario</label>
+								  	<input class="form-control" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{1,15}" type="text" name="userLog-up" maxlength="15" required="" >
+								</div>
+		    				</div>
+		    				<div class="col-xs-12 col-sm-6">
+								<div class="form-group label-floating">
+								  	<label class="control-label">Contraseña</label>
+								  	<input class="form-control" type="password" name="passwordLog-up" maxlength="50" required="">
+								</div>
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</fieldset>
+			    <p class="text-center" style="margin-top: 20px;">
+			    	<button type="submit" class="btn btn-success btn-raised btn-sm"><i class="zmdi zmdi-refresh"></i> Actualizar</button>
+			    </p>
+				<div class="RespuestaAjax"></div>
+		    </form>
+		</div>
+	</div>
 </div>
 
-<?php }else{ ?>
-<p>Error de cuenta</p>
+<?php   }else{  ?>
+<div class="alert alert-dismissible alert-warning text-center">
+	<button type="button" class="close" data-dismiss="alert">×</button>
+	<i class="zmdi zmdi-alert-triangle zmdi-hc-5x"></i>
+	<h4>¡Lo sentimos!</h4>
+	<p>No podemos mostrar la información de la cuenta debido a un error</p>
+</div>
 <?php
-    }
-    else:
+		}
+	else:
 ?>
-<p>Error de parametros</p>
+<div class="alert alert-dismissible alert-warning text-center">
+	<button type="button" class="close" data-dismiss="alert">×</button>
+	<i class="zmdi zmdi-alert-triangle zmdi-hc-5x"></i>
+	<h4>¡Lo sentimos!</h4>
+	<p>Ocurrió un error inesperado</p>
+</div>
 <?php endif; ?>
