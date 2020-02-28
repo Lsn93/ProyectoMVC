e758070cd42ca7ede75de879d29f0d81656aa305
Luis David Sáenz Jurado <luisdavidspider@gmail.com>
2020-02-28 15:20:55 -0600

    Modelo agregar empresa
    Modelo agregar empresa

--- modelos/empresaModelo.php
+++ modelos/empresaModelo.php
@@ -8,6 +8,16 @@
     class empresaModelo extends mainModel{
 
         protected function agregar_empresa_modelo($datos){
-
+            $query=mainModel::conectar()->prepare("INSERT INTO empresa(EmpresaCodigo,EmpresaNombre,EmpresaTelefono,EmpresaEmail,EmpresaDireccion,EmpresaDirector,EmpresaMoneda,EmpresaYear) VALUES(:Codigo,:Nombre,:Telefono,:Email,:Direccion,:Director,:Moneda,:Year)");
+            $query->bindParam(":Codigo",$datos['Codigo']);
+            $query->bindParam(":Nombre",$datos['Nombre']);
+            $query->bindParam(":Telefono",$datos['Telefono']);
+            $query->bindParam(":Email",$datos['Email']);
+            $query->bindParam(":Direccion",$datos['Direccion']);
+            $query->bindParam(":Director",$datos['Director']);
+            $query->bindParam(":Moneda",$datos['Moneda']);
+            $query->bindParam(":Year",$datos['Year']);
+            $query->execute();
+            return $query;
         }
     }
