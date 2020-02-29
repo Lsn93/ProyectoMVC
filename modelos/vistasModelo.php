5353dd6457adb5298485f588f586b9c4b4766d40
Luis David Sáenz Jurado <luisdavidspider@gmail.com>
2020-02-28 18:57:18 -0600

    Creando vista companyinfo
    Creando vista companyinfo

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -1,7 +1,7 @@
 <?php
     class vistasModelo{
         protected function obtener_vistas_modelo($vistas){
-            $listaBlanca=["adminlist","adminsearch","admin","bookconfig","bookinfo","book","catalog","categorylist","category","clientlist","clientsearch","client","companylist","company","home","myaccount","mydata","providerlist","provider","search"];
+            $listaBlanca=["adminlist","adminsearch","admin","bookconfig","bookinfo","book","catalog","categorylist","category","clientlist","clientsearch","client","companylist","company","home","myaccount","mydata","providerlist","provider","search","companyinfo"];
             if(in_array($vistas,$listaBlanca)){
                 if(is_file("./vistas/contenidos/".$vistas."-view.php")){
                     $contenido="./vistas/contenidos/".$vistas."-view.php";
